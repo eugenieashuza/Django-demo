54b6e3373a18bb5c57a4ee41fc680d371853e07d
eugenie ashuza <eugenieashuza@gmail.com>
2021-11-17 15:57:27 +0200

    add function category_index

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpRequest
+from app.models import Category
+from app.forms import CategoryForm
 
 # Create your views here.
 
 def home(request):
     return render(request,'app/home/home.html')
+
+def category_index(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/categories/index.html',
+        {
+            'categories': Category.objects.all()
+        }
+    )
