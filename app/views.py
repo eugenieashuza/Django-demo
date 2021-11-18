a2028502e1e9f0497d8c1b0090f53c9a15030210
eugenie ashuza <eugenieashuza@gmail.com>
2021-11-18 09:23:47 +0200

    create category_edit function for edit

--- app/views.py
+++ app/views.py
@@ -17,3 +17,45 @@ def category_index(request):
             'categories': Category.objects.all()
         }
     )
+def category_create(request):
+    assert isinstance(request, HttpRequest)
+    if request.method == "GET":
+        form = CategoryForm()
+        return render(
+            request,
+            'app/categories/create.html',
+            {
+                'form': form
+            }
+        )
+    else:
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/categories')
+    
+def category_edit(request, id):
+    assert isinstance(request, HttpRequest)
+    if request.method == "GET":
+        if id == 0:
+            form = CategoryForm()
+        else:
+            category = Category.objects.get(pk=id)
+            form = CategoryForm(instance=category)
+        return render(
+            request,
+            'app/categories/edit.html',
+            {
+                'form': form
+            }
+        )
+    else:
+        if id == 0:
+            form = CategoryForm(request.POST)
+        else:
+            category = Category.objects.get(pk=id)
+            form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+        return redirect('/categories')
+      
