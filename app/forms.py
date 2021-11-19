465fd237163881a88fa6b5896a81bd3b35f87abf
eugenie ashuza <eugenieashuza@gmail.com>
2021-11-19 10:47:40 +0200

    create product form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,14 @@
+from django.db.models import fields
 from django.forms import ModelForm
 from app.models import Category
+from app.models import Product
 
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
 
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
