b1b1611bdda81fe50f786429019748f37e51d0f0
eugenie ashuza <eugenieashuza@gmail.com>
2021-11-19 10:50:24 +0200

    create product:index,edit,delete function

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpRequest
-from app.models import Category
+from app.models import Category, Product
 from app.forms import CategoryForm
+from app.forms import ProductForm
 
 # Create your views here.
 
@@ -62,4 +63,63 @@ def category_edit(request, id):
 def category_delete(request, id):
     category = Category.objects.get(pk=id)
     category.delete()
-    return redirect('/categories')     
+    return redirect('/categories') 
+
+#products
+def products_index(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/produits/index.html',
+        {
+            'produits': Product.objects.all()
+        }
+    )
+    
+def product_create(request):
+    assert isinstance(request, HttpRequest)
+    if request.method == "GET":
+        form = ProductForm()
+        return render(
+            request,
+            'app/produits/create.html',
+            {
+                'form': form
+            }
+        )
+    else:
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/produits')
+    
+def product_edit(request, id):
+    assert isinstance(request, HttpRequest)
+    if request.method == "GET":
+        if id == 0:
+            form = ProductForm()
+        else:
+            product = Product.objects.get(pk=id)
+            form = ProductForm(instance=product)
+        return render(
+            request,
+            'app/produits/edit.html',
+            {
+                'form': form
+            }
+        )
+    else:
+        if id == 0:
+            form = ProductForm(request.POST)
+        else:
+            product = Product.objects.get(pk=id)
+            form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+        return redirect('/produits')
+    
+def product_delete(request, id):
+    product = Product.objects.get(pk=id)
+    product.delete()
+    return redirect('/produits') 
+    
