bc3d6f9c19db4900c926af35dbacdf763291e49c
eugenie ashuza <eugenieashuza@gmail.com>
2021-11-19 10:48:38 +0200

    create Product model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,11 @@ class Category(models.Model):
      
     def __str__(self):
         return self.category_name
+    
+class Product(models.Model):
+    product_name = models.CharField(unique=True, max_length=50)
+    category_id = models.ForeignKey(Category , on_delete=models.CASCADE)
+    unit_price = models.IntegerField()
+    def __str__(self):
+        return self.product_name
+    
